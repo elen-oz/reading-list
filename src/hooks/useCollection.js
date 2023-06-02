cae36a29265881d5d053c5ca35c9fe36591cb856
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-02 18:50:09 +0200

    firebase host

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,14 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 //firebase imports
 import { db } from '../firebase/config';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
 
-export const useCollection = (c) => {
+export const useCollection = (c, _q) => {
   const [documents, setDoduments] = useState(null);
+
+  //set up query
+  const q = useRef(_q).current;
 
   useEffect(() => {
     let ref = collection(db, c);
+
+    if (q) {
+      ref = query(ref, where(...q));
+    }
 
     const unsub = onSnapshot(ref, (snapshot) => {
       let results = [];
@@ -19,7 +26,7 @@ export const useCollection = (c) => {
     });
 
     return () => unsub();
-  }, [c]);
+  }, [c, q]);
 
   return { documents };
 };
