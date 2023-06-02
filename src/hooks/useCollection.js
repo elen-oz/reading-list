f588c8ed9324fbfeddbc6f23763a729c26eb830c
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-02 13:01:36 +0200

    feat: realtime collection data

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import { db } from '../firebase/config';
+
+//firebase imports
+import { collection, onSnapshot } from 'firebase/firestore';
+
+export const useCollection = (c) => {
+  const [documents, setDoduments] = useState(null);
+
+  useEffect(() => {
+    let ref = collection(db, c);
+
+    const unsub = onSnapshot(ref, (snapshot) => {
+      let results = [];
+      snapshot.docs.forEach((doc) => {
+        results.push({ id: doc.id, ...doc.data() });
+      });
+      setDoduments(results);
+    });
+
+    return () => unsub();
+  }, [c]);
+
+  return { documents };
+};
